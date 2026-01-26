af288f82a72710925c8e8b5bd62bece6db3ae45c
taher-dev <monmoyzx@gmail.com>
2026-01-26 16:31:25 +0600

    Add: numbers and characters allowed checkbox

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -23,17 +23,19 @@ function App() {
 
   return (
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-      <h1 className="text-white text-center my-3">Password Generator</h1>
+      <h1 className="text-white text-center my-3 text-xl">
+        Password Generator
+      </h1>
 
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
           type="text"
           value={password}
-          className="outline-none w-full py-1 px-3 bg-gray-700 text-white"
+          className="outline-none w-full py-1 pb-1.5 px-3 bg-gray-700 text-white"
           placeholder="password"
           readOnly
         />
-        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 pb-1.5 shrink-0 cursor-pointer">
           copy
         </button>
       </div>
@@ -45,8 +47,40 @@ function App() {
             min={4}
             max={20}
             className="cursor-pointer"
+            onChange={(e) => setLength(e.target.value)}
           />
           <label>Length: {length}</label>
+        </div>
+
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            className="cursor-pointer"
+            name="numberInput"
+            id="numberInput"
+            defaultChecked={numberAllowed}
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label className="cursor-pointer" htmlFor="numberInput">
+            Numbers
+          </label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            className="cursor-pointer"
+            name="charInput"
+            id="charInput"
+            defaultChecked={charAllowed}
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label className="cursor-pointer" htmlFor="charInput">
+            Characters
+          </label>
         </div>
       </div>
     </div>
